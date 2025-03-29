b4e9904bdd5be65348cd973d02b8ede3a2b61b85
MM_doma <MIKUS.MICHAL.BOJ@GMAIL.COM>
2025-03-29 14:44:18 +0100

    skratenie, zgeneralizovanie kodu

--- Rudno_Vent_Python/Rudno_Vent_Python.py
+++ Rudno_Vent_Python/Rudno_Vent_Python.py
@@ -1,33 +1,34 @@
 import machine, onewire, ds18x20, time
 from machine import Pin
 
+def teplomer(pin_port, poradove_c, kompenzacia):
+    ds_pin = machine.Pin(pin_port)
+    ds_sensor = ds18x20.DS18X20(onewire.OneWire(ds_pin))
+    ds_sensor.convert_temp()
+    roms = ds_sensor.scan()
+    print('Found DS devices: ', roms, f"\nOn pin{pin_port}")
+    teplota = ds_sensor.read_temp(roms[poradove_c])
+    return (teplota + kompenzacia)
+
+def spinac(temp_inside, temp_outside, hystereza):
+    if (temp_outside - (hystereza/2)) > temp_inside:
+        my_value = 0
+    elif (temp_outside + (hystereza/2)) < temp_inside:
+        my_value = 1
+    return my_value
+
 hystereza = 5
+value_moja = 0
 
 spinac = machine.Pin(18, Pin.OUT)
-
-ds_pin = machine.Pin(22)
-ds_sensor = ds18x20.DS18X20(onewire.OneWire(ds_pin))
-
-roms = ds_sensor.scan()
-print('Found DS devices: ', roms)
-
 sensor1_waterproof = bytearray(b'(\xffQ\xcb\xc1\x17\x04\xa6')
 sensor2_normal = bytearray(b'(\xff\xc6uQ\x17\x04\xec')
 
-spinac.value(1)
-time.sleep(1)
-spinac.value(0)
-
 while True:
-    ds_sensor.convert_temp()
-    time.sleep_ms(750)
-    temp_outside = ds_sensor.read_temp(sensor1_waterproof)
-    temp_inside = ds_sensor.read_temp(sensor2_normal)
-    print(temp_inside, "inside")
-    print(temp_outside, "outside")
-    if (temp_outside - (hystereza/2)) > temp_inside:
-        on_off = "off"
-    elif (temp_outside + (hystereza/2)) <= temp_inside:
-        on_off = "on"
-    print(on_off)
-    
+    #current_date = datetime.datetime.now().month
+    temp_outside = teplomer(22, 0, -0.5)
+    temp_inside = teplomer(22, 1, 0)
+    print("Inside:", temp_inside)
+    print("Outside:", temp_outside)
+    value_moja = spinac(temp_inside, temp_outside, hystereza)
+    spinac.value(value_moja)

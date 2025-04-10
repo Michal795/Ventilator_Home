35d4121eee1fa76b170c9f18f62077e03e181a5f
MM_doma <MIKUS.MICHAL.BOJ@GMAIL.COM>
2025-04-10 14:48:14 +0200

    pridal som PWM, zmenil som teplotnu hysterezu, lebo mi to nefungovalo

--- Rudno_Vent_Python/Rudno_Vent_Python.py
+++ Rudno_Vent_Python/Rudno_Vent_Python.py
@@ -1,27 +1,38 @@
-from machine import Pin
+from machine import Pin, PWM
 from time import sleep
 import dht
 
 inside = dht.DHT22(Pin(22))
 outside = dht.DHT22(Pin(21))
-spinac = machine.Pin(18, Pin.OUT)
-hystereza_temp = 3
+spinac = machine.Pin(18)
+pwm_spinac = machine.PWM(spinac)
+pwm_spinac.freq(50)
+pwm_spinac.duty_u16(0)
+
+hystereza_temp = 4
 hystereza_hum = 5
 hodnota_temp = 0
 hodnota_hum = 0
 norma_hum = 80
 
 
-def spinac_def(temp_inside, temp_outside, hystereza, pred_stav):
+def spinac_temp(temp_inside, temp_outside, hystereza, pred_stav):
     spinac_hodnota = pred_stav
-    if (temp_inside + (hystereza/2)) > temp_outside:
+    if (temp_inside + (hystereza/2)) < temp_outside:
         spinac_hodnota = 0
-    elif (temp_inside - (hystereza/2)) < temp_outside:
+        pwm_value = 0
+    elif (temp_inside - (hystereza/2)) > temp_outside:
         spinac_hodnota = 1
-    return  spinac_hodnota
+        pwm_value = 1024
+    else:
+        if pred_stav == 0:
+            pwm_value = ((temp_outside - (temp_inside - (hystereza/2))) / hystereza) * 1024
+        elif pred_stav == 1:
+            pwm_value = 1024 - (((temp_outside - (temp_inside - (hystereza/2))) / hystereza) * 1024)
+    return spinac_hodnota, int(round(pwm_value, 0))
 
 
-def spinac_def2(hystereza, humidity, pred_stav, norma):
+def spinac_hum(hystereza, humidity, pred_stav, norma):
     spinac_hodnota = pred_stav
     if (norma - (hystereza/2)) > humidity:
         spinac_hodnota = 1
@@ -31,29 +42,33 @@ def spinac_def2(hystereza, humidity, pred_stav, norma):
 
 
 while True:
-    sleep(1)
     inside.measure()
     outside.measure()
     temp_inside = float(inside.temperature())
     temp_outside = float(outside.temperature())
     hum_outside = float(outside.humidity())
+    '''
+    for i in range(25):
+        temp_outside = i
+        hodnota_temp, pwm_hodnota = spinac_temp(temp_inside, temp_outside, hystereza_temp, hodnota_temp)
+    for i in range(25, -1, -1):
+        temp_outside = i
+        hodnota_temp, pwm_hodnota = spinac_temp(temp_inside, temp_outside, hystereza_temp, hodnota_temp)
+    '''
     print("Inside temp:", temp_inside)
     print("Outside temp:", temp_outside)
     print("Outside humidity:", hum_outside)
-    hodnota_temp = spinac_def(temp_inside, temp_outside, hystereza_temp, hodnota_temp)
-    hodnota_hum = spinac_def2(hystereza_hum, hum_outside, hodnota_hum, norma_hum)
+    hodnota_temp, pwm_hodnota = spinac_temp(temp_inside, temp_outside, hystereza_temp, hodnota_temp)
+    hodnota_hum = spinac_hum(hystereza_hum, hum_outside, hodnota_hum, norma_hum)
     
-    if hodnota_temp == 1 and hodnota_hum == 1:
-        hodnota = 1
+    if hodnota_hum == 1:
+        pwm_spinac.duty_u16(pwm_hodnota)
     else:
-        hodnota = 0
+        pwm_spinac.duty_u16(0)
         
-    spinac.value(hodnota)
     print(f"Hodnota temp: {hodnota_temp}\nHodnota hum: {hodnota_hum}")
-    print(f"spinac: {hodnota}")
+    print(f"spinac: {pwm_hodnota}")
     
     ###print('Temperature: %3.1f C' %temp_inside)
     ###print('Humidity: %3.1f %%' %hum_outside)
     #hodnota = spinac_def(temp_inside, temp_outside, hystereza)
-
-
